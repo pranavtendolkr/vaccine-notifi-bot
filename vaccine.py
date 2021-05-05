0e17bc0caeb6e171bae653ab26460d5d53216734
Pranav Tendolkar <pranav@tendolkar.in>
2021-05-05 15:22:20 +0530

    Slight refectoring

--- vaccine.py
+++ vaccine.py
@@ -1,61 +1,53 @@
-import logging 
-from datetime import date
-import datetime
-import json
-import requests
+import logging
 import sys
+from datetime import datetime, timedelta
+import requests
 from requests import RequestException
 
-debug=False
+URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={0}&date={1}"
+DISTRICTS = [151, 152]
+# search for age limit less than:
+AGE_LIMIT = 45
 
 log = logging.getLogger()
-# make it print to the console.
-console = logging.FileHandler("debug.log")
-log.addHandler(console)
+log.addHandler(logging.FileHandler("debug.log"))
 log.setLevel(logging.INFO)
 
 
-if len(sys.argv) > 1:
-  debug=sys.argv[1]
-
-def print_available(res):
-    for center in res['centers']:
-      for session in center['sessions']:
-        if session['min_age_limit'] < 45:
-            if debug:
-                print(f" {center['name']}, {center['district_name']} , {session['date']}, slots available: {session['available_capacity']}")
-            if not debug and session['available_capacity'] != 0:
-                print(f" {center['name']}, {center['district_name']} , {session['date']}, slots available: {session['available_capacity']}")
-                #print(center['name'], center['district_name'], session['date'], session['available_capacity'])
+def print_availability(data, debug_flag):
+    for center in data['centers']:
+        for session in center['sessions']:
+            if session['min_age_limit'] < AGE_LIMIT:
+                if debug_flag:
+                    print(
+                        f" {center['name']}, {center['district_name']} , {session['date']}, slots available: {session['available_capacity']}")
+                if not debug_flag and session['available_capacity'] != 0:
+                    print(
+                        f" {center['name']}, {center['district_name']} , {session['date']}, slots available: {session['available_capacity']}")
 
 
-
-district_ids =[151,152]
-dates = []
-today = date.today()
-today = today.strftime("%d-%m-%Y")
-dates.append(today)
-today = datetime.date.today()
-next_monday = today + datetime.timedelta(days=-today.weekday(), weeks=1)
-next_monday=next_monday.strftime("%d-%m-%Y")
-dates.append(next_monday)
-for district_id in district_ids:
-    for date_check in dates:
-        try:
-            log.info(
-                "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s" % (
-                district_id, date_check))
-            res = requests.get(
-                "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s" % (
-                district_id, date_check))
-            res.raise_for_status()
-            print_available(res.json())
-        except RequestException as e:
-            log.exception(e)
-            if debug:
-                print("exception while fetching message: %s", e.response.text)
+def check_availability(districts, debug_flag):
+    dates = []
+    today = datetime.now().strftime("%d-%m-%Y")
+    next_monday = (datetime.now() + timedelta(days=-datetime.now().weekday(), weeks=1)).strftime("%d-%m-%Y")
+    dates.append(today)
+    dates.append(next_monday)
+    for district in districts:
+        for date in dates:
+            try:
+                log.info("checking for district %s for date %s", district, date)
+                response = requests.get(URL.format(district, date))
+                response.raise_for_status()
+                print_availability(data=response.json(), debug_flag=debug_flag)
+            except RequestException as ex:
+                log.error("Error calling API. Mostly due to rate limiting")
+                log.exception(ex)
+                if debug_flag:
+                    print("Failed to fetch. Response was: %s", ex.response.text)
 
 
-
-
-
+if __name__ == "__main__":
+    debug = False
+    if len(sys.argv) > 1:
+        debug = sys.argv[1]
+    check_availability(districts=DISTRICTS, debug_flag=debug)
